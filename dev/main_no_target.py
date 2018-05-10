48c117d683401f26bb5325033df7ad1ade9d6e99
Andreas Kvistad <ahk9339@gmail.com>
2018-05-10 14:37:57 +0200

    merge

--- dev/main_no_target.py
+++ dev/main_no_target.py
@@ -18,7 +18,7 @@ from torch.utils.data import DataLoader
 from utils.images import imageLoader as loader
 from utils.plot import loss_plot, percent_scatterplot as scatterplot
 from utils import transforms, tablewriter
-#from data.images.omniglot.omniglot_margin import OMNIGLOT_MARGIN
+from data.images.omniglot.omniglot_margin import OMNIGLOT_MARGIN
 from data.images.omniglot.omniglot import OMNIGLOT
 from data.images.mnist.MNIST import MNIST
 
@@ -75,19 +75,19 @@ parser.add_argument('--no-cuda', action='store_true', default=True,
                     help='enables CUDA training')
 
 # Checkpoint Loader:
-parser.add_argument('--load-checkpoint', default='pretrained/reinforced_ntm_wiped/checkpoint.pth.tar', type=str,
+parser.add_argument('--load-checkpoint', default='pretrained/reinforced_lstm_margin_r1_t5/checkpoint.pth.tar', type=str,
                     help='path to latest checkpoint (default: none)')
 
 # Checkpoint Loader:
-parser.add_argument('--load-best-checkpoint', default='pretrained/reinforced_ntm_wiped/best.pth.tar', type=str,
+parser.add_argument('--load-best-checkpoint', default='pretrained/reinforced_lstm_margin_r1_t5/best.pth.tar', type=str,
                     help='path to best checkpoint (default: none)')
 
 # Checkpoint Loader:
-parser.add_argument('--load-test-checkpoint', default='pretrained/reinforced_ntm_wiped/testpoint.pth.tar', type=str,
+parser.add_argument('--load-test-checkpoint', default='pretrained/reinforced_lstm_margin_r1_t5/testpoint.pth.tar', type=str,
                     help='path to best checkpoint (default: none)')
 
 # Network Name:
-parser.add_argument('--name', default='reinforced_ntm_wiped', type=str,
+parser.add_argument('--name', default='reinforced_lstm_margin_r1_t5', type=str,
                     help='name of file')
 
 # Seed:
@@ -175,8 +175,8 @@ if __name__ == '__main__':
         nof_classes = args.class_vector_size
         output_classes = nof_classes
 
-    LSTM = False
-    NTM = True
+    LSTM = True
+    NTM = False
     LRUA = False
 
     if LSTM:
@@ -192,8 +192,8 @@ if __name__ == '__main__':
 
     print("Loading trainingsets...")
     omniglot_loader = loader.OmniglotLoader(root, classify=False, partition=0.8, classes=True)
-        train_loader = torch.utils.data.DataLoader(
-        OMNIGLOT(root, train=True, transform=train_transform, download=True, omniglot_loader=omniglot_loader, classes=args.class_vector_size, episode_size=args.episode_size),
+    train_loader = torch.utils.data.DataLoader(
+        OMNIGLOT_MARGIN(root, train=True, transform=train_transform, download=True, omniglot_loader=omniglot_loader, classes=args.class_vector_size, episode_size=args.episode_size, margin_time=MARGIN_TIME, q_network=q_network),
         batch_size=args.mini_batch_size, shuffle=True, **kwargs)
     print("Loading testset...")
     if (not MNIST_TEST):
