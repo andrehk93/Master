5dbf581813ff1cfe8470b464a642eddd4e3ca04a
Andreas Kvistad <ahk9339@gmail.com>
2018-06-04 12:17:25 +0200

    omg2

--- dev/utils/tablewriter.py
+++ dev/utils/tablewriter.py
@@ -4,9 +4,11 @@ import shutil
 
 # Writes a table to file:
 def write_stats(requests, accuracy, penalty, folder, test=False):
-    filename = "results/plots/" + str(folder) + "table_file.txt"
-    stat_filename = "results/plots/" + str(folder) + "stat_file.txt"
-    dimensions = [50, 20, 20]
+    if (test):
+        filename = "results/plots/" + str(folder) + "test_table_file.txt"
+    else:
+        filename = "results/plots/" + str(folder) + "table_file.txt"
+    dimensions = [55, 31, 31]
     headers = ["Method", "Accuracy (%)", "Requests (%)"]
     method = "RL Prediction"
     if penalty == 0:
@@ -17,44 +19,15 @@ def write_stats(requests, accuracy, penalty, folder, test=False):
         else:
             method += "(Rinc = " + str(penalty) + ") - Training"
     specs = [accuracy, requests]
-    if (os.path.isfile(stat_filename)):
-        stat_file = open(stat_filename, "a")
-    else:
-        stat_file = open(stat_filename, "w")
-    stat_file.write(method + "\n")
-
-    # Averaging over 20 episodes:
-    for s in specs:
-        average = float(sum(s)/len(s))
-        stat_file.write(str(average)[0:5] + "\n")
-    stat_file.close()
-
-    # Reading from stat_file:
-    stats = {}
-    length = 3
-    with open(stat_filename, "r") as statistics:
-        i = 0
-        current_key = ""
-        for line in statistics:
-            if (i == 0):
-                if (line.rstrip() not in stats):
-                    stats[line.rstrip()] = [[], []]
-                current_key = line.rstrip()
-            elif (i < length):
-                stats[current_key][i-1].append(float(line.rstrip()))
-            i += 1
-            if (i == length):
-                i = 0
 
     stat_list = []
-    for k in stats.keys():
-        stat_list.append([])
-        stat_list[-1].append(k)
-        for v in stats[k]:
-            stat_list[-1].append(sum(v)/len(v))
-
-    print(stats)
-    print(stat_list)
+    j = 0
+    for s in specs:
+        if (j == 0):
+            stat_list.append(method)
+        average = float(sum(s)/len(s))
+        stat_list.append(average)
+        j += 1
 
     # Creating Line:
     table = ""
@@ -63,25 +36,27 @@ def write_stats(requests, accuracy, penalty, folder, test=False):
         line += "-"*d + "+"
     line += "\n"
 
-    if (os.path.isfile(filename)):
-        file = open(filename, "a")
+    file = open(filename, "w")
 
-    else:
-        file = open(filename, "w")
-
-        # HEADER
-        table += line
-        header = "|"
-        for i, d in enumerate(dimensions):
-            header += int((d/2) - int(len(headers[i])/2))*" " + headers[i] + int((d/2) - int(len(headers[i])/2))*" " + "|"
-        table += header
-        table += line
+    # HEADER
+    table += line
+    header = "|"
+    for i, d in enumerate(dimensions):
+        if (i == 0):
+            header += "\t\t\t" + headers[i] + "\t\t\t\t" + "|"
+        else:
+            header += "\t" + headers[i] + "\t\t" + "|"
+    table += header
+    table += line
 
     # BODY
-    for stat in stat_list:
+    for stat in range(0, len(stat_list), 3):
         table += "|"
-        for i, d in enumerate(dimensions):
-            table +=  int((d/2) - int(len(str(stat[i]))/2))*" " + str(stat[i]) + int((d/2) - int(len(str(stat[i]))/2))*" " + "|"
+        for i in range(3):
+            if (i == 0):
+                table +=  "\t" + str(stat_list[stat + i]) + "\t\t" + "|"
+            else:
+                table +=  "\t\t" + str(stat_list[stat + i])[0:5] + "\t\t" + "|"
         # END
         table += line
 
